55bb9dd1444920bf1e978d4c75dcbd13c54e48c9
rayhane111 <rayhanebeg@gmail.com>
2026-02-11 10:11:53 +0100

    Update index.js

--- bootcamp/exo02/index.js
+++ bootcamp/exo02/index.js
@@ -1,1 +1,15 @@
+import express from 'express' ;
+import users from './model/users';
+import patient from './model/patient';
 
+const app = express();
+const pport =3000 ;
+
+app.use(express.json());
+
+app.use('/users' , users);
+app.use('./patients' , patient);
+
+app.listen(port,() =>{
+}
+);
